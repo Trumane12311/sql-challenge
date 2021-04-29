3bdcfda04605252786eb7117e883d0b2e7f39e35
Trumane12311 <Trumane12311@gmail.com>
2021-04-29 19:05:41 -0400

    Almost Finished

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -1,6 +1,52 @@
-SELECT emp_no, last_name, first_name, sex FROM "Employees";
+-- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+SELECT e.emp_no, e.last_name, e.first_name, 
+		e.sex, s.salary FROM employees AS e
+INNER JOIN salary AS s ON
+e.emp_no = s.emp_no;
 
 
-SELECT * FROM public."Title"
-ORDER BY id;
+--2. List first name, last name, and hire date for employees who were hired in 1986.
+SELECT last_name, first_name, hire_date FROM employees
+WHERE hire_date between '1986-01-01' and '1986-12-31';
 
+
+--3. List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+SELECT d.dept_no, f.dept_name, e.emp_no, e.last_name, e.first_name FROM employees AS e
+INNER JOIN dept_managers AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS f ON
+d.dept_no = f.dept_no;
+
+
+--4. List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, f.dept_name FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS f ON
+d.dept_no = f.dept_no;
+
+
+--5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex FROM employees
+WHERE (first_name = 'Hercules' and last_name LIKE 'B%');
+
+
+--6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, f.dept_name FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS f ON
+d.dept_no = f.dept_no
+WHERE dept_name = 'Sales';
+
+
+--7. List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, f.dept_name FROM employees AS e
+INNER JOIN dept_emp AS d ON
+e.emp_no = d.emp_no
+INNER JOIN departments AS f ON
+d.dept_no = f.dept_no
+WHERE (dept_name = 'Sales' OR dept_name = 'Development');
+
+
+--8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
--- EmployeeSQL/data_engineering.sql
+++ EmployeeSQL/data_engineering.sql
@@ -1,64 +1,46 @@
-CREATE TABLE "Departments" (
-    "id" SERIAL   NOT NULL,
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "dept_name" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_Departments" PRIMARY KEY (
-        "dept_no"
-     )
+CREATE TABLE departments (
+    id SERIAL   NOT NULL,
+    dept_no VARCHAR(10) PRIMARY KEY  NOT NULL,
+    dept_name VARCHAR(255)   NOT NULL
 );
 
-CREATE TABLE "DeptEmployees" (
-    "id" SERIAL   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR(10)   NOT NULL,
-    CONSTRAINT "pk_DeptEmployees" PRIMARY KEY (
-        "emp_no"
-     )
+CREATE TABLE dept_emp (
+    id SERIAL   NOT NULL,
+    emp_no INT  NOT NULL,
+    dept_no VARCHAR(10)   NOT NULL
 );
 
-CREATE TABLE "DeptManagers" (
-    "id" SERIAL   NOT NULL,
-    "dept_no" VARCHAR(10)   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_DeptManagers" PRIMARY KEY (
-        "id","dept_no"
-     )
+CREATE TABLE dept_managers (
+    id SERIAL  PRIMARY KEY NOT NULL,
+    dept_no VARCHAR(10)   NOT NULL,
+    emp_no INT   NOT NULL
 );
 
-CREATE TABLE "Employees" (
-    "id" SERIAL   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR(20)   NOT NULL,
-    "birth_date" VARCHAR(20)   NOT NULL,
-    "first_name" VARCHAR(100)   NOT NULL,
-    "last_name" VARCHAR(100)   NOT NULL,
-    "sex" VARCHAR(30)   NOT NULL,
-    "hire_date" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_Employees" PRIMARY KEY (
-        "id"
-     )
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY  NOT NULL,
+    emp_no INT   NOT NULL,
+    emp_title_id VARCHAR(20)   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR(100)   NOT NULL,
+    last_name VARCHAR(100)   NOT NULL,
+    sex VARCHAR(30)   NOT NULL,
+    hire_date DATE   NOT NULL
 );
 
-CREATE TABLE "Salary" (
-    "id" SERIAL   NOT NULL,
-    "emp_no" INT   NOT NULL,
-    "salary" INT   NOT NULL,
-    CONSTRAINT "pk_Salary" PRIMARY KEY (
-        "id"
-     )
+CREATE TABLE salary (
+    id SERIAL PRIMARY KEY  NOT NULL,
+    emp_no INT   NOT NULL,
+    salary INT   NOT NULL
 );
 
-CREATE TABLE "Title" (
-    "id" SERIAL   NOT NULL,
-    "title_id" VARCHAR(10)   NOT NULL,
-    "title" VARCHAR(30)   NOT NULL,
-    CONSTRAINT "pk_Title" PRIMARY KEY (
-        "title_id"
-     )
+CREATE TABLE titles (
+    id SERIAL   NOT NULL,
+    title_id VARCHAR(10) PRIMARY KEY NOT NULL,
+    title VARCHAR(30)   NOT NULL
 );
 
-ALTER TABLE "DeptEmployees" ADD CONSTRAINT "fk_DeptEmployees_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE dept_emp ADD CONSTRAINT "fk_DeptEmployees_dept_no" FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
 
-ALTER TABLE "DeptManagers" ADD CONSTRAINT "fk_DeptManagers_dept_no" FOREIGN KEY("dept_no")
-REFERENCES "Departments" ("dept_no");
+ALTER TABLE dept_managers ADD CONSTRAINT "fk_DeptManagers_dept_no" FOREIGN KEY(dept_no)
+REFERENCES departments (dept_no);
